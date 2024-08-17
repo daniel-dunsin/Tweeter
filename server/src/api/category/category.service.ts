543d7c217d9c5f483e621c96ec419bcd67e079bb
daniel-dunsin <adejaredaniel12@gmail.com>
2024-08-17 19:00:53 +0100

    feat(server): seed categories

--- server/src/api/category/category.service.ts
+++ server/src/api/category/category.service.ts
@@ -1,10 +1,16 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, OnModuleInit } from '@nestjs/common';
 import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/shared/prisma/prisma.service';
 
+const CATEGORIES = require('../../../src/shared/seeds/categories.json');
+
 @Injectable()
-export class CategoryService {
+export class CategoryService implements OnModuleInit {
   constructor(private readonly prisma: PrismaService) {}
+
+  onModuleInit() {
+    this.seedCategories().then(() => console.log('Done'));
+  }
 
   async createCategory(data: Prisma.CategoryCreateInput) {
     return this.prisma.category.create({
@@ -46,5 +52,28 @@ export class CategoryService {
       where,
       include: { category: true },
     });
+  }
+
+  async seedCategories() {
+    const count = await this.prisma.category.count();
+
+    if (!count) {
+      const categories = CATEGORIES;
+      await Promise.all(
+        categories.map(async ({ subCategories, ...category }) => {
+          const { id } = await this.prisma.category.create({
+            data: category as Prisma.CategoryCreateInput,
+          });
+
+          return Promise.all(
+            subCategories.map(async (subcategory) =>
+              this.prisma.subCategory.create({
+                data: { categoryId: id, ...subcategory },
+              }),
+            ),
+          );
+        }),
+      );
+    }
   }
 }
