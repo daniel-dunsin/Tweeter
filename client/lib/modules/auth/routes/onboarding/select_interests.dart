60cd25605d119a168cc016b266f43ca83eababa9
daniel-dunsin <adejaredaniel12@gmail.com>
2024-08-23 22:35:47 +0100

    feat(client): select interests

--- client/lib/modules/auth/routes/onboarding/select_interests.dart
+++ client/lib/modules/auth/routes/onboarding/select_interests.dart
@@ -1,6 +1,5 @@
 import 'package:client/modules/categories/bloc/categories_bloc.dart';
 import 'package:client/modules/categories/models/category_model.dart';
-import 'package:client/modules/categories/models/subcategory_model.dart';
 import 'package:client/shared/theme/colors.dart';
 import 'package:client/shared/theme/index.dart';
 import 'package:client/shared/utils/network.dart';
