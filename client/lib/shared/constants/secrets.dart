a1e32a6ce434ebafaffac1d0053fe2e2692099e3
daniel-dunsin <adejaredaniel12@gmail.com>
2024-09-10 19:45:06 +0100

    fix: deployment

--- client/lib/shared/constants/secrets.dart
+++ client/lib/shared/constants/secrets.dart
@@ -1,4 +1,4 @@
 class AppSecrets {
-  static String serverUrl = "https://tweeter-jlos.onrender.com/api/v1";
+  static String serverUrl = "https://tweeter-backend-0b0o.onrender.com/api/v1";
   static String googleAndroidClientId = "971351874620-vk6nmu761eltbilqb96139o2gk8j1mtn.apps.googleusercontent.com";
 }
